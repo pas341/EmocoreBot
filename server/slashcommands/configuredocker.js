3c180210cfba31cddf6c6f67ccbc9d0af10e9d02
pas341 <philipasanne@hotmail.com>
2024-11-13 16:52:28 +0100

    update

--- server/slashcommands/configuredocker.js
+++ server/slashcommands/configuredocker.js
@@ -1,46 +1,71 @@
-var client, guild, query, util, self, msc, docker, perms;
+var client, guild, query, util, self, msc, docker, perms, config;
 
 module.exports = {
-    name: `createserver`,
+    name: `configuredocker`,
     description: `create a server`,
     type: 1,
     dm_permission: false,
     options: [
         {
-            name: `configureserver`,
+            name: `curseforge`,
             type: 1,
-            description: `Configure a server`,
+            description: `Configure a server curseforge docker container`,
             options: [
                 {
-                    name: `name`,
-                    description: `What is the name of the server you want to create`,
+                    name: `serverid`,
+                    description: `What is the id of the server you want to configure`,
+                    type: 4,
+                    required: true,
+                },
+                {
+                    name: `image`,
+                    description: `What is the docker image you want to use for the server`,
+                    type: 3,
+                    required: true,
+                    choices: null,
+                },
+                {
+                    name: `container`,
+                    description: `What is the name of the container`,
                     type: 3,
                     required: true,
                 },
                 {
-                    name: `iconurl`,
-                    description: `A Url to an icon for the server`,
+                    name: `maxram`,
+                    description: `how much ram does the server need in GB`,
+                    type: 4,
+                    required: true,
+                },
+                {
+                    name: `port`,
+                    description: `What is the main port for this server`,
+                    type: 4,
+                    required: true,
+                },
+                {
+                    name: `rconport`,
+                    description: `What is the rcon port for this server`,
+                    type: 4,
+                    required: true,
+                },
+                {
+                    name: `cfslug`,
+                    description: `What is the curseforge slug for the modpack`,
                     type: 3,
                     required: true,
                 },
                 {
-                    name: `modpackurl`,
-                    description: `A Url to the modpack you want to create a server for`,
+                    name: `fileid`,
+                    description: `What is the curseforge file id for the version of the modpack`,
                     type: 3,
                     required: true,
                 },
                 {
-                    name: `modpackversion`,
-                    description: `The Version of the modpack`,
+                    name: `cpus`,
+                    description: `What is the cpu configuration for this server`,
                     type: 3,
                     required: true,
                 },
-                {
-                    name: `minecraftversion`,
-                    description: `The minecraft version the modpack runs on`,
-                    type: 3,
-                    required: true,
-                }
             ],
             default_member_permissions: 0,
         },
@@ -55,26 +80,18 @@ module.exports = {
         perms = scripts.utils.permissionUtil;
         query = scripts.sql.query;
         util = scripts.util;
+        config = scripts.config;
         self = this;
 
         for (let option of this.options) {
             for (let o of option.options) {
                 if (o.name == `image`) {
-                    o.choices = await this.getServers();
+                    o.choices = await this.getImages();
                 }
             }
         }
     },
-    getServers: async () => {
-        let options = [];
-        let servers = await msc.getServerInfo();
-        for (let i of servers) {
-            let obj = { name: i.name, value: "" + i.id };
-            options.push(obj);
-        }
-        return options;
-    },
-    getServers: async () => {
+    getImages: async () => {
         let options = [];
         let images = await new Promise((resolve) => {
             query(`SELECT * FROM \`docker-images\``, [], async (error, results, fields) => {
@@ -82,7 +99,7 @@ module.exports = {
                     console.error(error);
                     resolve(null);
                 } else {
-                    resolve(results.length ? results[0] : null);
+                    resolve(results.length ? results : null);
                 }
             });
         });
@@ -103,8 +120,8 @@ module.exports = {
             let channel = interaction.member.guild.channels.cache.find(c => c.id === interaction.channelId);
             let subCommand = interaction.options._subcommand;
 
-            if (subCommand == `configureserver`) {
-                await self.configure_server(interaction, options, user, channel);
+            if (subCommand == `curseforge`) {
+                await self.curseforge(interaction, options, user, channel);
                 return;
             } else {
                 await self.sendErrorReply(interaction, `Command not found please contact <@228573762864283649>`, defered = 1);
@@ -115,19 +132,47 @@ module.exports = {
             await self.sendErrorReply(interaction, `An internal error occured please contact <@228573762864283649>`, defered = 1);
         }
     },
-    configure_server: async (interaction, options, user, channel) => {
-        let name = options.name;
-        let iconurl = options.iconurl;
-        let modpackurl = options.modpackurl;
-        let modpackversion = options.modpackversion;
-        let minecraftversion = options.minecraftversion;
+    curseforge: async (interaction, options, user, channel) => {
+        let serverid = options.serverid;
+        let containername = options.container;
+        let image = options.image;
+        let maxram = options.maxram;
+        let port = options.port;
+        let rconport = options.rconport;
+        let cfslug = options.cfslug;
+        let fileid = options.fileid;
+        let cpus = options.cpus;
+        let lram = options.maxram + 1;
 
-        let disocordChannelId = channel.id;
-        let channelCategoryId = channel.parentId;
+        let cf_api_key = await new Promise((resolve) => {
+            query(`SELECT * FROM \`server-config\` WHERE \`serverid\` = ?`, [config.server.serverid], async (error, results, fields) => {
+                if (error) {
+                    console.error(error);
+                    resolve(null);
+                } else {
+                    resolve(results.length ? results[0] : null);
+                }
+            });
+        });
+
+        let curseforge = {api_key: cf_api_key, slug: cfslug, file_id: fileid};
+
+        let obj = {
+            serverid: serverid,
+            image: image,
+            containername: containername,
+            maxram: maxram,
+            esport: port,
+            rport: rconport,
+            platform: `AUTO_CURSEFORGE`,
+            curseforgeconfig: curseforge,
+            cpus: cpus,
+            lram: lram
+        }
 
         let serverID = await new Promise((resolve) => {
-            query(`INSERT INTO \`minecraft-servers\` (\`name\`, \`iconurl\`, \`modpackurl\`, \`modpackversion\`, \`mcversion\`, \`discordchannel\`, \`discordcat\`) VALUES (?, ?, ?, ?, ?, ?, ?)`
-                , [name, iconurl, modpackurl, modpackversion, minecraftversion, disocordChannelId, channelCategoryId], async (error, results, fields) => {
+            query(`INSERT INTO \`minecraft-server-docker-config\``
+                , [obj], async (error, results, fields) => {
                 if (error) {
                     console.error(error);
                     resolve(null);
@@ -137,7 +182,7 @@ module.exports = {
                 }
             });
         });
-        await self.sendReply(interaction, `Server Creator`, `Server Configuration completed\nServerid: ${serverID}`, defered = 1);
+        await self.sendReply(interaction, `Docker Creator`, `Docker Configuration completed`, defered = 1);
     },
     sendReply: async (interaction, title, message, defered = 0) => {
         let e = { title: title, description: message, color: util.color.accent };
